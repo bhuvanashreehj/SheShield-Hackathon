5a7fcb22e229bbf1300c495ead62f0cfc34118f9
Bhuvana Shree H J <137808503+bhuvanashreehj@users.noreply.github.com>
2025-05-17 22:28:28 +0530

    Add files via upload
    // SheShield Core - Patent Pending
    import 'package:firebase_auth/firebase_auth.dart';
    // TODO: Implement voice stress analyzer

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+
+// SheShield Core - Patent Pending
+import 'package:firebase_auth/firebase_auth.dart';
+// TODO: Implement voice stress analyzer
+void main() {
+  print("Launching SheShield...");
+}
